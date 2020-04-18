c7677b84cebd3f32453291c80edbd1c5e27b3574
Erin Shields <elshields92@gmail.com>
2020-04-18 21:22:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,11 +19,12 @@ function whileLoop(n) {
 
 
 function doWhileLoop(num) {
+  let i = 0
   function incrementVariable() {
-    var i = 0
     i = i + 1;
     return i;
   }
+
   do {
     console.log("I run once regardless.")
   } while (incrementVariable() < num)
